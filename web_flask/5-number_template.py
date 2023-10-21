da72b28862f6fdecf9fa048e6dce10b16f8edf40
Judhunja <judxhunja@gmail.com>
2023-10-21 14:10:55 +0300

    rendering a html template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" This script configures a Flask web application with
+three routes """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    text_spaces = text.replace("_", " ")
+    return f"C {text_spaces}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text):
+    text_spaces = text.replace("_", " ")
+    return f"Python {text_spaces}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_if_int(n):
+    if type(n) == int:
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def template_if_int(n):
+    if type(n) == int:
+        return render_template("5-number.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
