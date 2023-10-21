1da49161e90977def84f0c6688950f362b10b66a
Judhunja <judxhunja@gmail.com>
2023-10-21 13:38:52 +0300

    adding default value for a route parameter

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This script configures a Flask web application with
+three routes """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    text_spaces = text.replace("_", " ")
+    return f"C {text_spaces}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text):
+    text_spaces = text.replace("_", " ")
+    return f"Python {text_spaces}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
